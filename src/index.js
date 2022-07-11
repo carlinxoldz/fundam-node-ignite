b6580e53410cad82fdadac7c3fe64f6d956c9470
carlos guilherme <carlinxold@hotmail.com>
2022-07-11 17:51:04 -0300

    5

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json())
 
 const customers = []
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const {cpf} = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+    if (!customer) {
+        return response.status(400).json({error: "not found"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
 
@@ -23,17 +37,29 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 })
 
-app.get("/statement", (request, response) => {
-    const {cpf} = request.headers;
+//app.use(verifyIfExistsAccountCPF);
 
-    const customer = customers.find(customer => customer.cpf === cpf);
-
-    if (!customer) {
-        return response.status(400).json({error: "not found"})
-    }
-
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const {customer} = request;
     return response.json(customer.statement)
 })
+
+app.post("/deposit",verifyIfExistsAccountCPF, (request, response) => {
+    const {description, amount} = request.body;
+
+    const {customer} = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
 
 app.listen(3333)
